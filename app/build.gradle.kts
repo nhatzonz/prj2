068de92a87b219df32d3a8878dd2b88b45b6f342
nhatzonz <156907693+nhatzonz@users.noreply.github.com>
2026-03-27 16:39:01 +0700

    fix cấu hình

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.prj2"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.prj2"
@@ -16,7 +12,6 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -40,6 +35,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.room.runtime)
+    annotationProcessor(libs.room.compiler)
+    implementation(libs.recyclerview)
+    implementation(libs.cardview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
